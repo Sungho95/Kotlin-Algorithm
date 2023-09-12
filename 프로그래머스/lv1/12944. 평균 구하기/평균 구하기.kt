b21ff22169782e696df78261f543b75b920cb3bf
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-12 19:51:49 +0900

    [level 1] Title: 평균 구하기, Time: 7.21 ms, Memory: 62.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.kt
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.kt
@@ -1,5 +1,28 @@
+// class Solution {
+//     fun solution(arr: IntArray): Double {
+//         return arr.average()
+//     }
+// }
+
 class Solution {
     fun solution(arr: IntArray): Double {
-        return arr.average()
+        val sum = arr.map {
+            it.toDouble()
+        }
+        .sum()
+        
+        return sum / arr.size
     }
 }
+
+// class Solution {
+//     fun solution(arr: IntArray): Double {
+//         var answer: Double = 0.0
+
+//         for (i in arr) {
+//             answer += i
+//         }
+
+//         return answer / arr.size
+//     }
+// }
