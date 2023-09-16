812c5f31f008cc85643e994aceec9adb52043da0
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-17 02:49:53 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.20 ms, Memory: 62.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -1,25 +1,29 @@
-// import kotlin.math.floor
-// import kotlin.math.pow
+import kotlin.math.floor
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class Solution {
+    fun solution(n: Long): Long {
+        val sqrt = sqrt(n.toDouble())
+
+        return if (sqrt == floor(sqrt)) {
+            (sqrt + 1).pow(2).toLong()
+        } else {
+            -1L
+        }
+    }
+}
+
 // import kotlin.math.sqrt
 
 // class Solution {
 //     fun solution(n: Long): Long {
-//         val sqrt = sqrt(n.toDouble())
-
-//         return if (sqrt == floor(sqrt)) {
-//             (sqrt + 1).pow(2).toLong()
+//         val sqrt = sqrt(n.toDouble()).toLong()
+        
+//         return if (sqrt * sqrt == n) {
+//             (sqrt + 1) * (sqrt + 1) 
 //         } else {
 //             -1L
 //         }
 //     }
 // }
-
-import kotlin.math.sqrt
-
-class Solution {
-    fun solution(n: Long): Long {
-        val sqrt = sqrt(n.toDouble()).toLong()
-        
-        return if (sqrt * sqrt == n) (sqrt + 1) * (sqrt + 1) else -1L
-    }
-}
