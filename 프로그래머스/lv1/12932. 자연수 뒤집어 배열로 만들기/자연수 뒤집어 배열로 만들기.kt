6f8e1b9a99fbdaf97ca17529571e1c9e27c4b65f
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-16 13:29:48 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 62.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.kt
@@ -1,12 +1,40 @@
+// class Solution {
+//     fun solution(n: Long): IntArray {
+//         val answer = n.toString()
+//             .reversed()
+//             .map {
+//                 it.toInt() - '0'.toInt()
+//             }
+//             .toIntArray()
+            
+//         return answer
+//     }
+// }
+
+
+// class Solution {
+//     fun solution(n: Long): IntArray {
+//         var answer = arrayListOf<Int>()
+//         var number = n
+        
+//         while (number > 0) {
+//             answer.add((number % 10).toInt())
+//             number /= 10
+//         }
+        
+//         return answer.toIntArray()
+//     }
+// }
+
 class Solution {
     fun solution(n: Long): IntArray {
-        val answer = n.toString()
-            .reversed()
-            .map {
-                it.toInt() - '0'.toInt()
-            }
-            .toIntArray()
-            
+        val s = n.toString()
+        var answer = IntArray(s.length)
+        
+        for (i in 0 until s.length) {
+            answer[i] = s[s.length - i - 1].toInt() - '0'.toInt()
+        }
+        
         return answer
     }
 }
