3e0709137d916f5684f0ec13dd22e7450d8b1e48
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-19 01:06:43 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 59.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.kt
@@ -1,14 +1,33 @@
+// class Solution {
+//     fun solution(x: Int): Boolean {
+//         val sum = x.toString()
+//             .sumBy {
+//                 it.code - '0'.code
+//             }.sum()
+
+//         return sum.let { x % it == 0 }
+//     }
+// }
+
+// class Solution {
+//     fun solution(x: Int): Boolean {
+//         val sum = x.toString()
+//             .sumBy { it.code - '0'.code }
+
+//         return sum.let { x % it == 0 }
+//     }
+// }
+
 class Solution {
     fun solution(x: Int): Boolean {
-        val sum = x.toString()
-            .map {
-            it.code - '0'.code 
-            }.sum()
-        
-        val answer = sum.let { 
-            x % it == 0
+        var sum = 0
+        var num = x
+
+        while (num > 0) {
+            sum += num % 10
+            num /= 10
         }
-        
-        return answer
+
+        return (x % sum == 0)
     }
 }
