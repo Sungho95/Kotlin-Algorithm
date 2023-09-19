8d73fb230bba7f7c44e532459b9e0a8b104f9bd8
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-20 01:16:12 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.08 ms, Memory: 61.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.kt
@@ -1,16 +1,42 @@
+// import kotlin.math.max
+// import kotlin.math.min
+
+// class Solution {
+//     fun solution(a: Int, b: Int): Long {
+//         return (min(a, b)..max(a, b))
+//             .map { it.toLong() }
+//             .sum()
+//     }
+// }
+
+// class Solution {
+//     fun solution(a: Int, b: Int): Long {
+//         return when {
+//             a == b -> a.toLong()
+//             a < b -> (a..b).map { it.toLong() }.sum()
+//             else -> (b..a).map { it.toLong() }.sum()
+//         }
+//     }
+// }
+
 import kotlin.math.max
 import kotlin.math.min
 
 class Solution {
     fun solution(a: Int, b: Int): Long {
-        return if (a == b) {
-            a.toLong()
-        } else {
-            (min(a, b)..max(a, b))
-                .map {
-                    it.toLong()
-                }
-                .sum()
+        var answer = 0L
+        
+        if (a == b) {
+            return a.toLong()
         }
+        
+        val min = min(a, b)
+        val max = max(a, b)
+        
+        for (i in min..max) {
+            answer += i
+        }
+        
+        return answer
     }
 }
