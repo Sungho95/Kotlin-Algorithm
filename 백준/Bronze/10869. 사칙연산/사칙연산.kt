2e5a6c85c0b614c27a35e1d85dde063b621db547
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-09-27 00:53:14 +0900

    [Bronze V] Title: 사칙연산, Time: 112 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val input = readln()
+    val a = input.split(" ")[0].toInt()
+    val b = input.split(" ")[1].toInt()
+    
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
