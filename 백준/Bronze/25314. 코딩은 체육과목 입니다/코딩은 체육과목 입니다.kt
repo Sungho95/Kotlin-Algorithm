93f0fb029c5c7ac1ce2e6cfe1dd35fc92690db68
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-10-17 22:24:10 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 104 ms, Memory: 13300 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.kt
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.kt
@@ -2,7 +2,7 @@ fun main() {
     val n = readln().toInt()
     val result = StringBuilder()
 
-    (0 until n / 4).forEach { _ -> result.append("long ") }
+    (0 until n / 4).forEach { result.append("long ") }
     result.append("int")
 
     println(result)
