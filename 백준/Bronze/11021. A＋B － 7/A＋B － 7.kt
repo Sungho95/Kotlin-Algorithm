8ffbee42745f99e5705138a33ffa28cd845e18af
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-10-19 23:40:26 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val n = readln().toInt()
+
+    repeat(n) {
+        val input = readln().split(" ")
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        println("Case #${it + 1}: ${a + b}")
+    }
+}
