d8dcf27a1aac8c1e4845cc4e97f36780c9d3f122
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-10-31 01:11:49 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 26952 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val n = readln().toInt()
+    val score = readln().split(" ").map { it.toDouble() }.toDoubleArray()
+    val m = score.max()
+
+    repeat(n) {
+        score[it] = (score[it] / m) * 100
+    }
+
+    println(score.average())
+}
