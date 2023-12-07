7c3aad7745b5d7fb3f755a856c0e743f01864d18
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2023-12-08 02:11:48 +0900

    [Bronze II] Title: 소수, Time: 148 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int min = n;
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (i < min) {
+                    min = i;
+                }
+            }
+        }
+
+        if (sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum + "\n" + min);
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
